4307c26742f8caecd6b50affba29c2a383b55934
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-12-29 23:41:00 -0500

    UPDATE navigation to work in all tree composables

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,18 +1,50 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 
 @Composable
-fun Home(modifier: Modifier) {
-    Text(
-        text = "Home Composable"
+fun Home(navigateToProfile: () -> Unit, modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Home Composable"
 
-    )
+        )
+
+        Button(
+            onClick = {
+                navigateToProfile()
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.primary_yellow)
+            )
+        ) {
+            
+            Text(
+                text = "Profile",
+                color = Color.DarkGray,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
 }
 
 
@@ -22,6 +54,6 @@ fun HomePreview() {
 
     LittleLemonTheme() {
 
-        Home(modifier = Modifier)
+        Home({} , modifier = Modifier)
     }
 }
